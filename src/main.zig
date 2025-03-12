6d1c4742c948bb376931f4616d1eab0c5bbd5f52
nolan freeman abbott <nolan@freemanabbott.com>
2025-03-12 15:15:10 -0500

    initial commit after bootstrap

---
+++ src/main.zig
@@ -0,0 +1,20 @@
+const rl = @import("raylib");
+
+pub fn main() anyerror!void {
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    defer rl.closeWindow();
+
+    rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+
+        rl.clearBackground(rl.Color.white);
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
+
+        rl.endDrawing();
+    }
+}
